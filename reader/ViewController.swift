4186d2a666cd185b9aa5b5ae08c91fa53f085732
karen's mac <wutingting823>
2019-09-30 14:57:55 +0800

    Initial Commit

---
+++ reader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reader
+//
+//  Created by JiuHua on 2019/9/30.
+//  Copyright © 2019 JiuHua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ readerTests/readerTests.swift
@@ -0,0 +1,34 @@
+//
+//  readerTests.swift
+//  readerTests
+//
+//  Created by JiuHua on 2019/9/30.
+//  Copyright © 2019 JiuHua. All rights reserved.
+//
+
+import XCTest
+@testable import reader
+
+class readerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
